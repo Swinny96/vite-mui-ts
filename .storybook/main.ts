9ac55f9b96295d93db144ccbae33e120f5de9f2c
Christopher Swinton <chris@theswintons.co.uk>
2023-07-19 11:06:03 +0100

    storybook/addon-styling postcss

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,10 +6,15 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling',
     {
       name: '@storybook/addon-styling',
-      options: {},
+      options: {
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: {
+          implementation: require.resolve('postcss'),
+        },
+      },
     },
   ],
   framework: {
